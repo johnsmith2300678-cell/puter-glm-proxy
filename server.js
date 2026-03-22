08763899bde2e7c6959e4bf8c90a35ce77a77c74
johnsmith2300678-cell <johnsmith2300678@gmail.com>
2026-03-22 15:45:45 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "10mb" }));a
 
 const PORT = process.env.PORT || 3000;
 
